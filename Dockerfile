216f4faad7a2c5879db8df1c8d30e81b68ca0994
charvotj <jakub.charvot@centrum.cz>
2024-04-27 02:03:16 +0200

    init commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the .NET 6 SDK image as the base image
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /source
+
+# Copy the project file and restore dependencies
+COPY *.csproj .
+RUN dotnet restore
+
+# Copy the remaining source code and build the application
+COPY . .
+RUN dotnet publish -c production -o /app --no-restore
+
+# Build the runtime image
+FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS runtime
+WORKDIR /app
+COPY --from=build /app .
+
+ENV ASPNETCORE_ENVIRONMENT=Development
+
+ENTRYPOINT ["dotnet", "AquaControlServerFrontend.dll"]
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using AquaControlServerFrontend.Helpers;
+using AquaControlServerFrontend.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AquaControlServerFrontend
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddScoped<IAccountService, AccountService>()
+                .AddScoped<IAlertService, AlertService>()
+                .AddScoped<IHttpService, HttpService>()
+                .AddScoped<ILocalStorageService, LocalStorageService>();
+
+            // configure http client
+            builder.Services.AddScoped(x => {
+                var apiUrl = new Uri(builder.Configuration["apiUrl"]);
+
+                // use fake backend if "fakeBackend" is "true" in appsettings.json
+                if (builder.Configuration["fakeBackend"] == "true")
+                {
+                    var fakeBackendHandler = new FakeBackendHandler(x.GetService<ILocalStorageService>());
+                    return new HttpClient(fakeBackendHandler) { BaseAddress = apiUrl };
+                }
+
+                return new HttpClient() { BaseAddress = apiUrl };
+            });
+
+            var host = builder.Build();
+
+            var accountService = host.Services.GetRequiredService<IAccountService>();
+            await accountService.Initialize();
+
+            await host.RunAsync();
+        }
+    }
+}
